e03c05b1fda61da7f717752943eacf17ba2837ac
yonghun16 <yonghun16@naver.com>
2025-06-09 15:38:56 +0900

    chor : 버튼 내 글자 고정폭 적용

--- src/shared/ui/Button.jsx
+++ src/shared/ui/Button.jsx
@@ -12,7 +12,7 @@ export function Button({
   ...props
 }) {
   // styles
-  const baseStyles = 'inline-flex items-center justify-center font-medium rounded-md transition';
+  const baseStyles = 'inline-flex items-center justify-center font-medium rounded-md transition whitespace-nowrap';
 
   const sizeStyles = {
     md: 'px-4 py-2 text-base',  // 보통 크기
