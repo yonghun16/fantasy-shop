93a30a008c28ecdb55c762239a20cc1633ab70e2
tony7401 <wogur520613@gmail.com>
2025-06-13 04:34:37 +0900

    feat: 로그인 페이지
    로그인 페이지

---
+++ src/pages/LoginPage/LoginImage.jsx
@@ -0,0 +1,20 @@
+import LoginImageCover from "../../assets/images/login-main.png";
+
+const LoginImage = () => {
+  return (
+    <div className="absolute inset-0 w-full h-full">
+      {/* 배경 이미지 + 흐림 효과 */}
+      <div
+        className="absolute inset-0 bg-cover bg-center blur-lg brightness-125"
+        style={{
+          backgroundImage: `url(${LoginImageCover})`,
+        }}
+      />
+
+      {/* 하얀 반투명 오버레이 */}
+      <div className="absolute inset-0 bg-white/40" />
+    </div>
+  );
+};
+
+export default LoginImage;
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,86 +1,78 @@
-import React, { useState } from "react";
-import clsx from "clsx";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { LuLock, LuMail } from "react-icons/lu";
 import logo from "../../assets/images/logo.png";
-import { useNavigate } from "react-router-dom";
-// import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import { InputBox } from "../../shared/ui/InputBox";
+import LoginImage from "./LoginImage";
 
-const LoginModal = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isSubmitted, setIsSubmitted] = useState(false);
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const emailChangeHandler = (e) => setEmail(e.target.value);
-  const passwordChangeHandler = (e) => setPassword(e.target.value);
-  const submitHandler = (e) => {
-    e.preventDefault();
-    setIsSubmitted(true);
-
-    if (email.trim() && password.trim()) {
-      alert("로그인 성공!");
-
-      const from = location.state?.from?.pathname || "/";
+  const onSubmit = (data) => {
+    alert("로그인 성공!");
+    const from = location.state?.from;
+    if (typeof from === "string") {
       navigate(from);
+    } else {
+      navigate("/");
     }
   };
 
   const registerHandler = () => navigate("/register");
 
-  const emailIsInvalid = isSubmitted && email.trim() === "";
-  const passwordIsInvalid = isSubmitted && password.trim() === "";
-
   return (
-    <div className="fixed inset-0 bg-none bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg p-8 w-96 shadow-xl">
-        <form onSubmit={submitHandler} className="flex flex-col gap-6">
+    <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden">
+      {/* 배경 이미지 */}
+      <LoginImage />
+
+      {/* 로그인 폼 */}
+      <div className="relative z-20 bg-white rounded-lg p-8 w-[350px] shadow-xl border border-gray-300">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
           <div className="flex flex-col items-center text-center gap-1 font-bold text-2xl">
             <img src={logo} alt="로그인 메인 이미지" className="w-24" />
             <p>판타지 쇼핑몰에</p>
             <p>오신걸 환영합니다.</p>
             <p>용사여!</p>
           </div>
-          <div>
-            <p className="font-bold">Email</p>
-            <div className="relative">
-              {/* <EnvelopeIcon className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2" /> */}
-              <input
-                type="email"
-                placeholder="이메일을 입력하세요"
-                value={email}
-                onChange={emailChangeHandler}
-                className={clsx(
-                  "w-full border p-2 pl-10 rounded",
-                  emailIsInvalid ? "border-red-500" : "border-gray-300"
-                )}
-              />
-            </div>
-            {emailIsInvalid && (
-              <p className="text-red-500 text-sm">이메일을 입력하세요.</p>
+
+          <div className="relative">
+            <LuMail className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 -translate-y-1/2 z-10" />
+            <InputBox
+              {...register("email", { required: "이메일을 입력하세요." })}
+              type="email"
+              placeholder="이메일을 입력하세요"
+              className={`w-full border p-2 pl-10 rounded ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
             )}
           </div>
 
-          <div>
-            <p className="font-bold">Password</p>
-            <div className="relative">
-              {/* <LockClosedIcon className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2" /> */}
-              <input
-                type="password"
-                placeholder="패스워드를 입력하세요"
-                value={password}
-                onChange={passwordChangeHandler}
-                className={clsx(
-                  "w-full border p-2 pl-10 rounded",
-                  passwordIsInvalid ? "border-red-500" : "border-gray-300"
-                )}
-              />
-            </div>
-            {passwordIsInvalid && (
-              <p className="text-red-500 text-sm">비밀번호를 입력하세요.</p>
+          <div className="relative">
+            <LuLock className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 -translate-y-1/2 z-10" />
+            <InputBox
+              {...register("password", { required: "비밀번호를 입력하세요." })}
+              type="password"
+              placeholder="패스워드를 입력하세요"
+              className={`w-full border p-2 pl-10 rounded ${
+                errors.password ? "border-red-500" : "border-gray-300"
+              }`}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password.message}</p>
             )}
           </div>
 
           <button
-            onClick={submitHandler}
             type="submit"
             className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
           >
@@ -103,4 +95,4 @@ const LoginModal = () => {
   );
 };
 
-export default LoginModal;
+export default Login;
