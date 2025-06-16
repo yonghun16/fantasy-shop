65315fbed03c0e98e710d8c0c91dc96e187347af
tony7401 <wogur520613@gmail.com>
2025-06-16 13:06:36 +0900

    feat:로그인 모바일 페이지
    로그인 모바일 페이지

--- src/pages/LoginPage/LoginBackImage.jsx
+++ src/pages/LoginPage/LoginBackImage.jsx
@@ -1,13 +1,22 @@
-import LoginImageCover from "../../assets/images/login-main.png";
+import loginMobileMain from "../../assets/images/login-mobile-main.png";
+import loginMain from "../../assets/images/login-main.png";
 
 const LoginBackImage = () => {
   return (
     <div className="absolute inset-0 w-full h-full">
-      {/* 배경 이미지 + 불투명 효과 */}
+      {/* 모바일 배경 */}
       <div
-        className="absolute inset-0 bg-cover bg-center opacity-50"
+        className="absolute inset-0 bg-cover bg-center opacity-50 md:hidden"
         style={{
-          backgroundImage: `url(${LoginImageCover})`,
+          backgroundImage: `url(${loginMobileMain})`,
+        }}
+      />
+
+      {/* 데스크탑 배경 */}
+      <div
+        className="hidden md:block absolute inset-0 bg-cover bg-center opacity-50"
+        style={{
+          backgroundImage: `url(${loginMain})`,
         }}
       />
 
