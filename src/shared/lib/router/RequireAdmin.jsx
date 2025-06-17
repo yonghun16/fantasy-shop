bb7a8a289fb8fefc063b39462bc9982359798f1e
yonghun16 <yonghun16@naver.com>
2025-06-17 20:50:41 +0900

    fix : 어드민 버그 수정

--- src/shared/lib/router/RequireAdmin.jsx
+++ src/shared/lib/router/RequireAdmin.jsx
@@ -7,7 +7,7 @@ const RequireAdmin = () => {
   const isAuth = useSelector((state) => state.user.isAuth);
   const userData = useSelector((state) => state.user.userData);
 
-  if (!isAuth || !userData?.userPk || !userData?.isAdmin) {
+  if (!isAuth || !userData?.userPk || !userData?.admin) {
     return <Navigate to="/" state={{ from: location }} replace />;
   }
 
