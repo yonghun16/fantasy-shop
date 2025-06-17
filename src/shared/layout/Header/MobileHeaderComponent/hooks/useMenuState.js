79ef73fd14f25210c0febb4478954e067efb8b4e
yonghun16 <yonghun16@naver.com>
2025-06-17 17:05:22 +0900

    chor : 헤더컴포넌트 오타수정

--- src/shared/layout/Header/MobileHeaderComponent/hooks/useMenuState.js
+++ src/shared/layout/Header/MobileHeaderComponent/hooks/useMenuState.js
@@ -10,7 +10,7 @@ const useMenuState = (locate) => {
       setMenuState(locate)
     } else if (location.pathname.startsWith("/cart")) {
       setMenuState("장바구니")
-    } else if (location.pathname.startsWith("/mypage")) {
+    } else if (location.pathname.startsWith("/myprofile")) {
       setMenuState("내프로필")
     } else if (location.pathname.startsWith("/register")) {
       setMenuState("회원가입")
