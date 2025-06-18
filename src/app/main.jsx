3b1a8625e05e8cb145eb0539f56d19c9f9712c25
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-06-19 02:00:50 +0900

    perf : 로딩 속도
    상품목록, 상세페이지 로딩 속도 개선

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,16 +2,24 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor,store } from "../store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../store/store";
+
+/* import React Query */
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 /* import router */
 import router from "./router";
 
+/* create QueryClient instance */
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor} >
-      <RouterProvider router={router} />
+    <PersistGate loading={null} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </PersistGate>
   </Provider>
 );
