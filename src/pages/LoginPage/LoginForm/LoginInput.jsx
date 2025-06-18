04ecd0cf4454afac0dca7d389d0d250da06b428a
yonghun16 <yonghun16@naver.com>
2025-06-18 18:01:38 +0900

    refactor : 로그인 리펙토링

---
+++ src/pages/LoginPage/LoginForm/LoginInput.jsx
@@ -0,0 +1,29 @@
+/* components */
+import { InputBox } from "../../../shared/ui/InputBox";
+
+/* assets */
+import { LuMail, LuLock } from "react-icons/lu";
+
+
+const icons = {
+  mail: <LuMail />,
+  lock: <LuLock />,
+};
+
+
+const LoginInput = ({ type, label, placeholder, icon, register, error, autoComplete }) => (
+  <div className="relative">
+    <InputBox
+      type={type}
+      label={<span className="hidden md:block">{label}</span>}
+      icon={icons[icon]}
+      placeholder={placeholder}
+      className="w-full h-12"
+      autoComplete={autoComplete}
+      {...register}
+    />
+    {error && <p className="text-rose-500 text-sm">{error}</p>}
+  </div>
+);
+
+export default LoginInput;
---
+++ src/pages/LoginPage/RegisterLink.jsx
@@ -0,0 +1,10 @@
+const RegisterLink = () => (
+  <div className="flex flex-col md:flex-row mt-10 justify-center items-center gap-4 text-sm text-center">
+    <p>처음이신가요?</p>
+    <a href="register" className="text-indigo-500 hover:underline">
+      회원가입
+    </a>
+  </div>
+);
+
+export default RegisterLink;
