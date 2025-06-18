d8b79ce0c7fdc6ae336cf84f8361e0e3108a76d7
yonghun16 <yonghun16@naver.com>
2025-06-19 07:02:38 +0900

    chor : 들여쓰기 정리

--- src/features/user/api/registerUser.js
+++ src/features/user/api/registerUser.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../../shared/api/axios";
 
-// 회원 가입
+// 회원 가입 API
 export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (body, thunkAPI) => {
@@ -13,7 +13,7 @@ export const registerUser = createAsyncThunk(
       return response.data
     } catch (error) {
       const errorMessage = error?.response?.data || error.message || "Register failed";
-      console.error("Login Error:", errorMessage);
+      console.error("Register Error:", errorMessage);
       return thunkAPI.rejectWithValue(error.response.data || error.message)
     }
   }
