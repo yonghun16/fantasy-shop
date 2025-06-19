3702762e8cbf6e055dfa3188b76c8b1fa17732f3
ines012 <celeste07102@gmail.com>
2025-06-19 18:02:56 +0800

    feat: 아이템 카테고리 별 성능 활성화 기능 구현

--- src/pages/UploadProductPage/ItemStats.jsx
+++ src/pages/UploadProductPage/ItemStats.jsx
@@ -1,45 +1,57 @@
-import React from 'react';
-
 import { LuSword, LuShield, LuWand } from "react-icons/lu";
 import { GiPocketBow } from "react-icons/gi";
 
 const stats = [
-  { label: '데미지', icon: <LuSword size={24} /> },
-  { label: '방어력', icon: <LuShield size={24} /> },
-  { label: '명중력', icon: <GiPocketBow size={24} /> },
-  { label: '마법력', icon: <LuWand size={24} /> },
+  { label: "데미지", icon: <LuSword size={24} />, category: "sword" },
+  { label: "방어력", icon: <LuShield size={24} />, category: "shield" },
+  { label: "명중력", icon: <GiPocketBow size={24} />, category: "bow" },
+  ,
+  { label: "마법력", icon: <LuWand size={24} />, category: "wand" },
 ];
 
-export default function ItemStats({ register }) {
+const ItemStats = ({ register, watch }) => {
+  const selectedCategory = watch("category");
+
   return (
     <div>
       <label className="block font-semibold mb-3">아이템 성능</label>
       <div className="grid grid-cols-2 gap-4">
-        {stats.map(({ label, icon }) => (
-          <div key={label} className="border border-gray-200 rounded-md p-3">
-            <div className="flex flex-col items-center gap-2 font-semibold mb-2">
-              <div>{icon}</div>
-              <div>{label}</div>
-            </div>
-            <input
-              type="number"
-              placeholder="0-999"
-              className="
+        {stats.map(({ label, icon, category }) => {
+          const isActive = selectedCategory === category;
+
+          return (
+            <div key={label} className="border border-gray-200 rounded-md p-3">
+              <div className="flex flex-col items-center gap-2 font-semibold mb-2">
+                <div>{icon}</div>
+                <div>{label}</div>
+              </div>
+              <input
+                type="number"
+                placeholder="0-999"
+                disabled={!isActive}
+                className={`
                 w-full text-center 
                 border border-gray-200 rounded-md p-2 
+                ${
+                  isActive
+                    ? "border-indigo-500 bg-white"
+                    : "border-gray-200 bg-gray-100 text-gray-400"
+                }
                 hover:bg-gray-50
-                focus:border-indigo-500 focus:outline-none focus:bg-indigo-50"
-              min={0}
-              max={999}
-              {...register(`stats.${label}`, {
-                valueAsNumber: true,
-                min: 0,
-                max: 999,
-              })}
-            />
-          </div>
-        ))}
+                focus:border-indigo-500 focus:outline-none focus:bg-indigo-50`}
+                min={0}
+                max={999}
+                {...register(`stats.${label}`, {
+                  valueAsNumber: true,
+                  min: 0,
+                  max: 999,
+                })}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
-}
+};
+export default ItemStats;
--- src/pages/UploadProductPage/index.jsx
+++ src/pages/UploadProductPage/index.jsx
@@ -1,20 +1,19 @@
 /* import library */
-import { useState } from 'react';
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 /* import component */
-import ItemBasicInfo from './ItemBasicInfo';
-import ItemStats from './ItemStats';
-import ItemCategory from './ItemCategory';
-import { Button } from '../../shared/ui/Button'
+import ItemBasicInfo from "./ItemBasicInfo";
+import ItemStats from "./ItemStats";
+import ItemCategory from "./ItemCategory";
+import { Button } from "../../shared/ui/Button";
 
 /* import assets */
 import { LuUpload } from "react-icons/lu";
 
-
 /* UI */
 const ItemForm = () => {
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState("");
 
   // react-hook-form
   const {
@@ -24,7 +23,7 @@ const ItemForm = () => {
     formState: { errors },
     reset,
   } = useForm({
-    mode: "onTouched",    // 사용자가 input을 터치하고 벗어났을 때 유효성 검사 실행
+    mode: "onTouched", // 사용자가 input을 터치하고 벗어났을 때 유효성 검사 실행
   });
 
   // onSubmit Handler
@@ -38,33 +37,31 @@ const ItemForm = () => {
     <div className="max-w-5xl mx-auto m-18">
       <h1 className="text-2xl font-bold mb-8 text-center">아이템 등록</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)} >
-        <div className="
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div
+          className="
           grid grid-cols-1 md:grid-cols-2 gap-8
-          px-12 py-16 mb-12 md:m-12 border-0 md:border border-gray-200 rounded-md">
+          px-12 py-16 mb-12 md:m-12 border-0 md:border border-gray-200 rounded-md"
+        >
           {/* 왼쪽 패널 */}
           <ItemBasicInfo register={register} errors={errors} />
 
           {/* 오른쪽 패널 */}
           <div className="space-y-6">
             <ItemCategory register={register} watch={watch} />
-            <ItemStats register={register} />
+            <ItemStats register={register} watch={watch} />
           </div>
-
         </div>
 
         {/* 등록 버튼 */}
         <div className="flex justify-center mt-8 md:col-span-2">
-          <Button
-            type="submit"
-            icon={<LuUpload />}
-            className="w-60"
-          >아이템 등록하기</Button>
+          <Button type="submit" icon={<LuUpload />} className="w-60">
+            아이템 등록하기
+          </Button>
         </div>
       </form>
-
     </div>
   );
-}
+};
 
 export default ItemForm;
