6be5a2cc26d14fbcbf7103e3c9854e7817e6bda2
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-06-25 23:39:29 +0900

    chore : 헤더 로고
    상품 검색 중 로고 클릭 시 상품 목록 초기화 시키기 위해 홈에서 로고 클릭한다면 새로고침 진행

--- src/shared/layout/Header/DesktopHeader/Logo.jsx
+++ src/shared/layout/Header/DesktopHeader/Logo.jsx
@@ -1,25 +1,32 @@
-import { Link } from 'react-router-dom'
+import { useNavigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 /* assets */
-import logoImg from '../../../../assets/images/logo.webp'
-
+import logoImg from "../../../../assets/images/logo.webp";
 
 const Logo = () => {
-  const shopName = useSelector((state) => state.company.companyData.companyName);
-  return (
-    <Link
-      className="flex items-center gap-2"
-      to="/"
-    >
-      <img
-        src={logoImg}
-        alt="logo"
-        className='w-10 h-10'
-      />
-      <p>{shopName}</p>
-    </Link>
-  )
-}
+  const shopName = useSelector(
+    (state) => state.company.companyData.companyName
+  );
+  const navigate = useNavigate();
+  const location = useLocation();
 
-export default Logo
+  const handleClick = (e) => {
+    e.preventDefault(); // 기본 Link 이동 막기
+
+    if (location.pathname === "/") {
+      window.location.reload(); // 홈이면 새로고침
+    } else {
+      navigate("/"); // 아니면 홈으로 이동
+    }
+  };
+
+  return (
+    <a href="/" className="flex items-center gap-2" onClick={handleClick}>
+      <img src={logoImg} alt="logo" className="w-10 h-10" />
+      <p>{shopName}</p>
+    </a>
+  );
+};
+
+export default Logo;
