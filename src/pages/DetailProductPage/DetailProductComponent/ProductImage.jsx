67742bed85c43232f81da9fec56f49e8a0ec9b4c
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-06-27 01:13:01 +0900

    fix : 이미지 경로 관련
    이유 모를 오류? 버그? 커밋 후 머지 중 뭔가 꼬인듯 함

--- src/pages/DetailProductPage/DetailProductComponent/ProductImage.jsx
+++ src/pages/DetailProductPage/DetailProductComponent/ProductImage.jsx
@@ -3,7 +3,7 @@ import { getImageUrl } from "../../../shared/utils/getImageUrl";
 const ProductImage = ({ src, alt }) => (
   <div className="flex-1 flex justify-center items-center w-full h-full">
     <img
-      src={getImageUrl("150x150")}
+      src={getImageUrl(src)}
       alt={alt}
       className="max-w-full max-h-[600px] w-auto h-auto object-contain rounded-md"
     />
