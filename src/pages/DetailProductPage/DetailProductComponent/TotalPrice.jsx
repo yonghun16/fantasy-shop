3b491bdc65dbbb0d1175ad6a3ae20d883a0ad1f5
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-06-26 21:33:15 +0900

    chore : 가격 수정
    천 단위마다 , 표시

--- src/pages/DetailProductPage/DetailProductComponent/TotalPrice.jsx
+++ src/pages/DetailProductPage/DetailProductComponent/TotalPrice.jsx
@@ -1,6 +1,11 @@
 const TotalPrice = ({ price, count }) => (
   <div className="text-right text-lg text-indigo-600 font-semibold mb-3">
-    총 금액: {(price * count).toFixed(2)} G
+    총 금액:{" "}
+    {(price * count).toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}{" "}
+    G
   </div>
 );
 
